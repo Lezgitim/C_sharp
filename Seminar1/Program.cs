1001bdbb9e5d4f532017250547d1f0ecbf09dfbc
Rinad <921837@gmail.com>
2022-08-22 17:17:12 +0300

    zadacha2

---
+++ Seminar1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/* 
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine ($"Большим числом является: {a}");
+    Console.WriteLine ($"Меньшим числом является: {b}");
+}
+else
+{
+    Console.WriteLine ($"Большим числом являетcя: {b}");
+    Console.WriteLine ($"Меньшим числом является: {a}");
+}
+
+if (a == b)
+{
+    Console.WriteLine ($"Числа: {a} и {b} равны между собой, большее или меньшее число отсутствует.");
+}
+*/
+
+
+
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/* 
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b; 
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+ Console.WriteLine ($"Максимальным числом из введенных чисел является: {max}");
+*/
